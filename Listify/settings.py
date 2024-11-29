a187c9a384951d00e883a4b9e32c95a4107efccf
Gabriel Rigoni <rigonigabriel12@gmail.com>
2024-11-29 14:12:09 -0300

    vercel 4

--- Listify/settings.py
+++ Listify/settings.py
@@ -115,7 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Diretório onde estão os arquivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   # Diretório para onde os arquivos serão coletados
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
